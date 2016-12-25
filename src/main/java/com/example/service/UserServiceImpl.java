8faef87dc2c481e0c96bcc962ccb9bc99cabf75f
Shigeharu Matsumoto <m_shige1979@r2.dion.ne.jp>
2016-12-25 18:09:54 +0900

    データ取得時に権限を設定

--- src/main/java/com/example/service/UserServiceImpl.java
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -1,6 +1,11 @@
 package com.example.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,15 +24,23 @@ public class UserServiceImpl implements UserDetailsService{
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
+		// ユーザーIDをチェック
 		if (StringUtils.isEmpty(username)) {
 			throw new UsernameNotFoundException("");
 		}
 		
+		// ユーザー情報を取得
 		User user = userRepository.findByUsername(username);
 		if (user == null) {
 			throw new UsernameNotFoundException("");
 		}
 		
+		// 権限を設定
+		Collection<GrantedAuthority> authorityList = new ArrayList<>();
+		authorityList.add(new SimpleGrantedAuthority("ROLE_USER"));
+		user.setAuthorities(authorityList);
+		
+		// 返却
 		return user;
 	}
 
